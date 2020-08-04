e612636b5c46f8ce207d3c74460346d3cc158e5d
Ethan Mann <emann@triaxtec.com>
2020-08-04 13:03:55 -0400

    Regenerated golden-master

--- end_to_end_tests/golden-master/my_test_api_client/models/an_enum_value.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class AnEnumValue(str, Enum):
-    FIRST_VALUE = "FIRST_VALUE"
-    SECOND_VALUE = "SECOND_VALUE"
--- end_to_end_tests/golden-master/my_test_api_client/models/an_enum_value1.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class AnEnumValue1(str, Enum):
-    DIFFERENT = "DIFFERENT"
-    OTHER = "OTHER"
