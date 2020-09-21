0e6266c269c14c97932cb3abec16a2e99580f8e5
Ethan Mann <emann@triaxtec.com>
2020-09-21 13:53:06 -0400

    Regenerated golden record one more time

--- end_to_end_tests/golden-record/my_test_api_client/client.py
+++ end_to_end_tests/golden-record/my_test_api_client/client.py
@@ -7,7 +7,7 @@ import attr
 class Client:
     """ A class for keeping track of data related to the API """
 
-    base_url: str = attr.ib()
+    base_url: str
     cookies: Dict[str, str] = attr.ib(factory=dict, kw_only=True)
     headers: Dict[str, str] = attr.ib(factory=dict, kw_only=True)
     timeout: float = attr.ib(5.0, kw_only=True)
