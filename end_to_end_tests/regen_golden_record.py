c8ae42dbdcdebde7da221dbb38ef37a4967837f2
Dylan Anthony <danthony@triaxtec.com>
2020-11-07 10:56:15 -0700

    Fix custom template E2E test

--- end_to_end_tests/regen_golden_record.py
+++ end_to_end_tests/regen_golden_record.py
@@ -11,17 +11,18 @@ if __name__ == "__main__":
     runner = CliRunner()
     openapi_path = Path(__file__).parent / "openapi.json"
 
-    output_path = Path.cwd() / "my-test-api-client"
-
     custom = len(sys.argv) >= 2 and sys.argv[1] == "custom"
     if custom:
         gr_path = Path(__file__).parent / "golden-record-custom"
+        output_path = Path.cwd() / "custom-e2e"
+        config_path = Path(__file__).parent / "custom_config.yml"
     else:
         gr_path = Path(__file__).parent / "golden-record"
+        output_path = Path.cwd() / "my-test-api-client"
+        config_path = Path(__file__).parent / "config.yml"
 
     shutil.rmtree(gr_path, ignore_errors=True)
     shutil.rmtree(output_path, ignore_errors=True)
-    config_path = Path(__file__).parent / "config.yml"
 
     if custom:
         result = runner.invoke(
