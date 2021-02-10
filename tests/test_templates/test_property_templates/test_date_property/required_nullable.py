3fd7c8d6cf77730434702de2c5c1dc9cb3c9d986
Forest Tong <forest@benchling.com>
2021-02-10 10:04:04 -0500

    Fix tests

--- tests/test_templates/test_property_templates/test_date_property/required_nullable.py
+++ tests/test_templates/test_property_templates/test_date_property/required_nullable.py
@@ -3,7 +3,7 @@ from typing import cast, Union
 
 from dateutil.parser import isoparse
 some_source = date(2020, 10, 12)
-some_destination = some_source.isoformat() if some_source else None
+some_destination = some_source.isoformat() if some_source else None 
 a_prop = None
 _a_prop = some_destination
 if _a_prop is not None:
