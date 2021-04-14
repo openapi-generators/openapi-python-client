d4eb9ff0831bc44f8d34a371ac34a8345dc277d7
Forest Tong <forest@benchling.com>
2021-04-14 12:56:46 -0400

    Fix

--- end_to_end_tests/golden-record/my_test_api_client/models/model_from_one_of.py
+++ end_to_end_tests/golden-record/my_test_api_client/models/model_from_one_of.py
@@ -1,8 +1,6 @@
-from typing import Any, Dict, List, Type, TypeVar, Union
+from typing import Any, Dict, List, Type, TypeVar
 
 import attr
-
-from ..types import UNSET, Unset
 
 T = TypeVar("T", bound="ModelFromOneOf")
 
@@ -11,35 +9,20 @@ T = TypeVar("T", bound="ModelFromOneOf")
 class ModelFromOneOf:
     """  """
 
-    a_sub_property: Union[Unset, str] = UNSET
-    another_sub_property: Union[Unset, str] = UNSET
     additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
 
     def to_dict(self) -> Dict[str, Any]:
-        a_sub_property = self.a_sub_property
-        another_sub_property = self.another_sub_property
 
         field_dict: Dict[str, Any] = {}
         field_dict.update(self.additional_properties)
         field_dict.update({})
-        if a_sub_property is not UNSET:
-            field_dict["a_sub_property"] = a_sub_property
-        if another_sub_property is not UNSET:
-            field_dict["another_sub_property"] = another_sub_property
 
         return field_dict
 
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
         d = src_dict.copy()
-        a_sub_property = d.pop("a_sub_property", UNSET)
-
-        another_sub_property = d.pop("another_sub_property", UNSET)
-
-        model_from_one_of = cls(
-            a_sub_property=a_sub_property,
-            another_sub_property=another_sub_property,
-        )
+        model_from_one_of = cls()
 
         model_from_one_of.additional_properties = d
         return model_from_one_of
