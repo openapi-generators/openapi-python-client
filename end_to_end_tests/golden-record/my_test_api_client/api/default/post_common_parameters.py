21f872e91c005ff8f705ef5efc66597e8bba78e4
Guillaume Gelin <contact@ramnes.eu>
2021-04-26 00:41:53 +0200

    feat: Support common parameters for paths (#376). Thanks @ramnes!
    * Support common parameters
    
    https://swagger.io/docs/specification/describing-parameters/#common
    
    * test: Added e2e snapshot test for common path parameters
    
    Co-authored-by: Dylan Anthony <43723790+dbanty@users.noreply.github.com>
    Co-authored-by: Dylan Anthony <contact@dylananthony.com>

---
+++ end_to_end_tests/golden-record/my_test_api_client/api/default/post_common_parameters.py
@@ -0,0 +1,72 @@
+from typing import Any, Dict, Union
+
+import httpx
+
+from ...client import Client
+from ...types import UNSET, Response, Unset
+
+
+def _get_kwargs(
+    *,
+    client: Client,
+    common: Union[Unset, str] = UNSET,
+) -> Dict[str, Any]:
+    url = "{}/common_parameters".format(client.base_url)
+
+    headers: Dict[str, Any] = client.get_headers()
+    cookies: Dict[str, Any] = client.get_cookies()
+
+    params: Dict[str, Any] = {
+        "common": common,
+    }
+    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+
+    return {
+        "url": url,
+        "headers": headers,
+        "cookies": cookies,
+        "timeout": client.get_timeout(),
+        "params": params,
+    }
+
+
+def _build_response(*, response: httpx.Response) -> Response[None]:
+    return Response(
+        status_code=response.status_code,
+        content=response.content,
+        headers=response.headers,
+        parsed=None,
+    )
+
+
+def sync_detailed(
+    *,
+    client: Client,
+    common: Union[Unset, str] = UNSET,
+) -> Response[None]:
+    kwargs = _get_kwargs(
+        client=client,
+        common=common,
+    )
+
+    response = httpx.post(
+        **kwargs,
+    )
+
+    return _build_response(response=response)
+
+
+async def asyncio_detailed(
+    *,
+    client: Client,
+    common: Union[Unset, str] = UNSET,
+) -> Response[None]:
+    kwargs = _get_kwargs(
+        client=client,
+        common=common,
+    )
+
+    async with httpx.AsyncClient() as _client:
+        response = await _client.post(**kwargs)
+
+    return _build_response(response=response)
