57c42a886a61f546b24803306f35839b9d169826
Constantinos Symeonides <constantinos.symeonides@mindfoundry.ai>
2021-05-19 12:33:26 +0100

    fix: Non-JSON non-file multipart props need to be text/plain

--- end_to_end_tests/golden-record/my_test_api_client/models/body_upload_file_tests_upload_post.py
+++ end_to_end_tests/golden-record/my_test_api_client/models/body_upload_file_tests_upload_post.py
@@ -4,6 +4,9 @@ from typing import Any, Dict, List, Optional, Tuple, Type, TypeVar, Union, cast
 
 import attr
 
+from ..models.body_upload_file_tests_upload_post_additional_property import (
+    BodyUploadFileTestsUploadPostAdditionalProperty,
+)
 from ..models.body_upload_file_tests_upload_post_some_nullable_object import (
     BodyUploadFileTestsUploadPostSomeNullableObject,
 )
@@ -11,6 +14,7 @@ from ..models.body_upload_file_tests_upload_post_some_object import BodyUploadFi
 from ..models.body_upload_file_tests_upload_post_some_optional_object import (
     BodyUploadFileTestsUploadPostSomeOptionalObject,
 )
+from ..models.different_enum import DifferentEnum
 from ..types import UNSET, File, FileJsonType, Unset
 
 T = TypeVar("T", bound="BodyUploadFileTestsUploadPost")
@@ -28,6 +32,10 @@ class BodyUploadFileTestsUploadPost:
     some_number: Union[Unset, float] = UNSET
     some_array: Union[Unset, List[float]] = UNSET
     some_optional_object: Union[Unset, BodyUploadFileTestsUploadPostSomeOptionalObject] = UNSET
+    some_enum: Union[Unset, DifferentEnum] = UNSET
+    additional_properties: Dict[str, BodyUploadFileTestsUploadPostAdditionalProperty] = attr.ib(
+        init=False, factory=dict
+    )
 
     def to_dict(self) -> Dict[str, Any]:
         some_file = self.some_file.to_tuple()
@@ -50,7 +58,14 @@ class BodyUploadFileTestsUploadPost:
 
         some_nullable_object = self.some_nullable_object.to_dict() if self.some_nullable_object else None
 
+        some_enum: Union[Unset, str] = UNSET
+        if not isinstance(self.some_enum, Unset):
+            some_enum = self.some_enum.value
+
         field_dict: Dict[str, Any] = {}
+        for prop_name, prop in self.additional_properties.items():
+            field_dict[prop_name] = prop.to_dict()
+
         field_dict.update(
             {
                 "some_file": some_file,
@@ -68,6 +83,8 @@ class BodyUploadFileTestsUploadPost:
             field_dict["some_array"] = some_array
         if some_optional_object is not UNSET:
             field_dict["some_optional_object"] = some_optional_object
+        if some_enum is not UNSET:
+            field_dict["some_enum"] = some_enum
 
         return field_dict
 
@@ -80,8 +97,8 @@ class BodyUploadFileTestsUploadPost:
         if not isinstance(self.some_optional_file, Unset):
             some_optional_file = self.some_optional_file.to_tuple()
 
-        some_string = self.some_string
-        some_number = self.some_number
+        some_string = self.some_string if self.some_string is UNSET else (None, str(self.some_string), "text/plain")
+        some_number = self.some_number if self.some_number is UNSET else (None, str(self.some_number), "text/plain")
         some_array: Union[Unset, Tuple[None, str, str]] = UNSET
         if not isinstance(self.some_array, Unset):
             _temp_some_array = self.some_array
@@ -97,7 +114,14 @@ class BodyUploadFileTestsUploadPost:
             else None
         )
 
+        some_enum: Union[Unset, Tuple[None, str, str]] = UNSET
+        if not isinstance(self.some_enum, Unset):
+            some_enum = (None, str(self.some_enum.value), "text/plain")
+
         field_dict: Dict[str, Any] = {}
+        for prop_name, prop in self.additional_properties.items():
+            field_dict[prop_name] = (None, json.dumps(prop.to_dict()), "application/json")
+
         field_dict.update(
             {
                 "some_file": some_file,
@@ -115,6 +139,8 @@ class BodyUploadFileTestsUploadPost:
             field_dict["some_array"] = some_array
         if some_optional_object is not UNSET:
             field_dict["some_optional_object"] = some_optional_object
+        if some_enum is not UNSET:
+            field_dict["some_enum"] = some_enum
 
         return field_dict
 
@@ -152,6 +178,13 @@ class BodyUploadFileTestsUploadPost:
         else:
             some_nullable_object = BodyUploadFileTestsUploadPostSomeNullableObject.from_dict(_some_nullable_object)
 
+        _some_enum = d.pop("some_enum", UNSET)
+        some_enum: Union[Unset, DifferentEnum]
+        if isinstance(_some_enum, Unset):
+            some_enum = UNSET
+        else:
+            some_enum = DifferentEnum(_some_enum)
+
         body_upload_file_tests_upload_post = cls(
             some_file=some_file,
             some_object=some_object,
@@ -161,6 +194,30 @@ class BodyUploadFileTestsUploadPost:
             some_array=some_array,
             some_optional_object=some_optional_object,
             some_nullable_object=some_nullable_object,
+            some_enum=some_enum,
         )
 
+        additional_properties = {}
+        for prop_name, prop_dict in d.items():
+            additional_property = BodyUploadFileTestsUploadPostAdditionalProperty.from_dict(prop_dict)
+
+            additional_properties[prop_name] = additional_property
+
+        body_upload_file_tests_upload_post.additional_properties = additional_properties
         return body_upload_file_tests_upload_post
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> BodyUploadFileTestsUploadPostAdditionalProperty:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: BodyUploadFileTestsUploadPostAdditionalProperty) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
