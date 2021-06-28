605c246a120b02fb8a5aea35e4fb3d469fa0fddf
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2021-06-27 18:17:03 -0600

    feat: Allow custom templates for API and endpoint `__init__` files. [#442] Thanks @p1-ra!
    * templatize api,endpoint init files
    
    * e2e / add {api_init.py.jina, endpoint_init.py.jinja} custom templates tests
    
    * expose utils module to jinja2
    
    * test_custom_templates / replace jinja2 marcro with call to utils module
    
    * task / regen / regen custom template golden record
    
    * test_end_to_end / correct typing
    
    * endoint __init__ template / factorize propagated vars
    
    * templates / globaly propagate package metadata vars
    
    * test___init__.py / correct templates API call breaking changes
    
    * Update test_end_to_end.py,openapi_python_client/__init__.py : various imprv
    
    Co-authored-by: Dylan Anthony <43723790+dbanty@users.noreply.github.com>
    
    * regen templates golden records (rebased on main)
    
    Co-authored-by: Nementon <nementon@badwolf.fr>
    Co-authored-by: p1-ra <aurelien.roose@p1sec.com>
    Co-authored-by: p1-ra <18233250+p1-ra@users.noreply.github.com>

---
+++ end_to_end_tests/custom-templates-golden-record/my_test_api_client/api/__init__.py
@@ -0,0 +1,21 @@
+""" Contains methods for accessing the API """
+
+from typing import Type
+
+from my_test_api_client.api.default import DefaultEndpoints
+from my_test_api_client.api.parameters import ParametersEndpoints
+from my_test_api_client.api.tests import TestsEndpoints
+
+
+class MyTestApiClientApi:
+    @classmethod
+    def tests(cls) -> Type[TestsEndpoints]:
+        return TestsEndpoints
+
+    @classmethod
+    def default(cls) -> Type[DefaultEndpoints]:
+        return DefaultEndpoints
+
+    @classmethod
+    def parameters(cls) -> Type[ParametersEndpoints]:
+        return ParametersEndpoints
--- end_to_end_tests/regen_golden_record.py
+++ end_to_end_tests/regen_golden_record.py
@@ -1,12 +1,16 @@
 """ Regenerate golden-record """
+import filecmp
+import os
 import shutil
+import tempfile
 from pathlib import Path
 
 from typer.testing import CliRunner
 
 from openapi_python_client.cli import app
 
-if __name__ == "__main__":
+
+def regen_golden_record():
     runner = CliRunner()
     openapi_path = Path(__file__).parent / "openapi.json"
 
@@ -24,3 +28,52 @@ if __name__ == "__main__":
     if result.exception:
         raise result.exception
     output_path.rename(gr_path)
+
+
+def regen_custom_template_golden_record():
+    runner = CliRunner()
+    openapi_path = Path(__file__).parent / "openapi.json"
+    tpl_dir = Path(__file__).parent / "test_custom_templates"
+
+    gr_path = Path(__file__).parent / "golden-record"
+    tpl_gr_path = Path(__file__).parent / "custom-templates-golden-record"
+
+    output_path = Path(tempfile.mkdtemp())
+    config_path = Path(__file__).parent / "config.yml"
+
+    shutil.rmtree(tpl_gr_path, ignore_errors=True)
+
+    os.chdir(str(output_path.absolute()))
+    result = runner.invoke(
+        app, ["generate", f"--config={config_path}", f"--path={openapi_path}", f"--custom-template-path={tpl_dir}"]
+    )
+
+    if result.stdout:
+        generated_output_path = output_path / "my-test-api-client"
+        for f in generated_output_path.glob("**/*"):  # nb: works for Windows and Unix
+            relative_to_generated = f.relative_to(generated_output_path)
+            gr_file = gr_path / relative_to_generated
+            if not gr_file.exists():
+                print(f"{gr_file} does not exist, ignoring")
+                continue
+
+            if not gr_file.is_file():
+                continue
+
+            if not filecmp.cmp(gr_file, f, shallow=False):
+                target_file = tpl_gr_path / relative_to_generated
+                target_dir = target_file.parent
+
+                target_dir.mkdir(parents=True, exist_ok=True)
+                shutil.copy(f"{f}", f"{target_file}")
+
+        shutil.rmtree(output_path, ignore_errors=True)
+
+    if result.exception:
+        shutil.rmtree(output_path, ignore_errors=True)
+        raise result.exception
+
+
+if __name__ == "__main__":
+    regen_golden_record()
+    regen_custom_template_golden_record()
