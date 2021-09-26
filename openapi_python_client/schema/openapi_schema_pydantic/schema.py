f5b5ce8ad5c55127f9eeade2868bd03e5f3504d4
Dylan Anthony <contact@dylananthony.com>
2021-09-25 21:20:34 -0600

    fix: Don't crash when a null is in an enum (fixes #500)

--- openapi_python_client/schema/openapi_schema_pydantic/schema.py
+++ openapi_python_client/schema/openapi_schema_pydantic/schema.py
@@ -35,7 +35,7 @@ class Schema(BaseModel):
     maxProperties: Optional[int] = Field(default=None, ge=0)
     minProperties: Optional[int] = Field(default=None, ge=0)
     required: Optional[List[str]] = Field(default=None, min_items=1)
-    enum: Optional[List[Any]] = Field(default=None, min_items=1)
+    enum: Optional[List[Union[str, int]]] = Field(default=None, min_items=1)
     type: Optional[DataType] = Field(default=None)
     allOf: Optional[List[Union[Reference, "Schema"]]] = None
     oneOf: List[Union[Reference, "Schema"]] = []
