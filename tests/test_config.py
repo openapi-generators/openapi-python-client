f0bb516ba677c4f12964d8680a20b240a8666a64
Pierre Tardy <pierre.tardy@renault.com>
2021-10-11 14:00:04 +0200

    config Support json containing tabs
    json is not technically a subset of yaml because yaml don't support tabs while json does
    
    Signed-off-by: Pierre Tardy <pierre.tardy@renault.com>

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,8 @@
+import json
 import pathlib
+
+import pytest
+import yaml
 
 from openapi_python_client.config import Config
 
@@ -28,3 +32,28 @@ def test_load_from_path(mocker):
     assert config.project_name_override == "project-name"
     assert config.package_name_override == "package_name"
     assert config.package_version_override == "package_version"
+
+
+DATA = {"class_overrides": {"Class1": {"class_name": "ExampleClass", "module_name": "example_module"}}}
+
+
+def json_with_tabs(d):
+    return json.dumps(d, indent=4).replace("    ", "\t")
+
+
+@pytest.mark.parametrize(
+    "filename,dump",
+    [
+        ("example.yml", yaml.dump),
+        ("example.json", json.dumps),
+        ("example.yaml", yaml.dump),
+        ("example.json", json_with_tabs),
+    ],
+)
+def test_load_filenames(tmp_path, filename, dump):
+    yml_file = tmp_path.joinpath(filename)
+    with open(yml_file, "w") as f:
+        f.write(dump(DATA))
+
+    config = Config.load_from_path(yml_file)
+    assert config.class_overrides == DATA["class_overrides"]
