b01db03349eb83e4c3ffdd1cc70f7eb2a9c6dae8
Dylan Anthony <43723790+dbanty@users.noreply.github.com>
2021-10-16 17:59:38 -0600

    fix: properly support JSON OpenAPI documents and config files [#488, #509, #515]. Thanks @tardyp and @Gelbpunkt! (#515)
    Co-authored-by: Pierre Tardy <pierre.tardy@renault.com>

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,27 +1,38 @@
-import pathlib
+import json
+
+import pytest
+import yaml
 
 from openapi_python_client.config import Config
 
 
-def test_load_from_path(mocker):
-    from openapi_python_client import utils
+def json_with_tabs(d):
+    return json.dumps(d, indent=4).replace("    ", "\t")
 
+
+@pytest.mark.parametrize(
+    "filename,dump",
+    [
+        ("example.yml", yaml.dump),
+        ("example.json", json.dumps),
+        ("example.yaml", yaml.dump),
+        ("example.json", json_with_tabs),
+    ],
+)
+def test_load_from_path(tmp_path, filename, dump):
+    yml_file = tmp_path.joinpath(filename)
     override1 = {"class_name": "ExampleClass", "module_name": "example_module"}
     override2 = {"class_name": "DifferentClass", "module_name": "different_module"}
-    safe_load = mocker.patch(
-        "yaml.safe_load",
-        return_value={
-            "field_prefix": "blah",
-            "class_overrides": {"Class1": override1, "Class2": override2},
-            "project_name_override": "project-name",
-            "package_name_override": "package_name",
-            "package_version_override": "package_version",
-        },
-    )
-    fake_path = mocker.MagicMock(autospec=pathlib.Path)
+    data = {
+        "field_prefix": "blah",
+        "class_overrides": {"Class1": override1, "Class2": override2},
+        "project_name_override": "project-name",
+        "package_name_override": "package_name",
+        "package_version_override": "package_version",
+    }
+    yml_file.write_text(dump(data))
 
-    config = Config.load_from_path(fake_path)
-    safe_load.assert_called()
+    config = Config.load_from_path(yml_file)
     assert config.field_prefix == "blah"
     assert config.class_overrides["Class1"] == override1
     assert config.class_overrides["Class2"] == override2
