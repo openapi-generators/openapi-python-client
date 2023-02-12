3bb99c71fe290f2635d959bf162b6ba8ef2ada17
Daniele Esposti <daniele@withplum.com>
2023-02-12 22:17:17 +0000

    Added support of follow HTTP redirects

--- end_to_end_tests/golden-record/my_test_api_client/api/parameter_references/get_parameter_references_path_param.py
+++ end_to_end_tests/golden-record/my_test_api_client/api/parameter_references/get_parameter_references_path_param.py
@@ -39,6 +39,7 @@ def _get_kwargs(
         "headers": headers,
         "cookies": cookies,
         "timeout": client.get_timeout(),
+        "follow_redirects": client.follow_redirects,
         "params": params,
     }
 
