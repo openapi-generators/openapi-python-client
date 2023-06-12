9ded7f06af90f95eeee9ff30045b13d5a600ae65
Marcin Rudolf <rudolfix@rudolfix.org>
2023-06-12 14:21:11 +0200

    shows data types in questionary

--- openapi_python_client/cli_endpoint_selection.py
+++ openapi_python_client/cli_endpoint_selection.py
@@ -8,7 +8,11 @@ from .parser.endpoint_collection import Endpoints, Endpoint
 def questionary_endpoint_selection(endpoints: Endpoints) -> Set[str]:
     """Endpoint selection with questionary. Returns a Set of endpoint names"""
     choices: List[questionary.Choice] = []
+    prev_table_name = ""
     for endpoint in endpoints.all_endpoints_to_render:
+        if prev_table_name != endpoint.table_name:
+            choices.append(questionary.Separator(f"\n{endpoint.table_name} endpoints:\n"))
+        prev_table_name = endpoint.table_name
         # for tag, collection in endpoints.endpoints_by_tag.items():
         #     if not collection.endpoints_to_render:
         #         continue
