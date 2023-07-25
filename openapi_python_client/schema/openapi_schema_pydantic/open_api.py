b2587001f0f9748ac62e42a5cd1acd6863283954
Kristinn Vikar <kristinn@syndis.is>
2023-07-25 20:57:22 +0000

    Fix OpenAPI model rebuilding

--- openapi_python_client/schema/openapi_schema_pydantic/open_api.py
+++ openapi_python_client/schema/openapi_schema_pydantic/open_api.py
@@ -6,6 +6,9 @@ from pydantic import BaseModel, ConfigDict
 from .components import Components
 from .external_documentation import ExternalDocumentation
 from .info import Info
+
+# Required to update forward ref after object creation
+from .path_item import PathItem
 from .paths import Paths
 from .security_requirement import SecurityRequirement
 from .server import Server
@@ -29,3 +32,6 @@ class OpenAPI(BaseModel):
     externalDocs: Optional[ExternalDocumentation] = None
     openapi: Union[Literal["3.0.0"], Literal["3.0.1"], Literal["3.0.2"], Literal["3.0.3"]]
     model_config = ConfigDict(extra="allow")
+
+
+OpenAPI.model_rebuild()
