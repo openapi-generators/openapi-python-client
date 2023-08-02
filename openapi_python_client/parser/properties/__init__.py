1a93f6a198bbfd601f027f4cfa2808afb38cd278
Kristinn Vikar <kristinn@syndis.is>
2023-08-02 09:59:22 +0000

    Add missing type to union in build_enum_property

--- openapi_python_client/parser/properties/__init__.py
+++ openapi_python_client/parser/properties/__init__.py
@@ -355,7 +355,7 @@ def build_enum_property(
     name: str,
     required: bool,
     schemas: Schemas,
-    enum: Union[List[Optional[str]], List[Optional[int]]],
+    enum: Union[List[Optional[str]], List[Optional[int]], List[Optional[bool]]],
     parent_name: Optional[str],
     config: Config,
 ) -> Tuple[Union[EnumProperty, NoneProperty, PropertyError], Schemas]:
