9a9f54f6046c9dea3346e5806e378edf478a976b
Dylan Anthony <dbanty@users.noreply.github.com>
2023-10-14 12:36:39 -0600

    test: Ignore `.ruff_cache` in golden records

--- end_to_end_tests/test_end_to_end.py
+++ end_to_end_tests/test_end_to_end.py
@@ -26,7 +26,7 @@ def _compare_directories(
     """
     first_printable = record.relative_to(Path.cwd())
     second_printable = test_subject.relative_to(Path.cwd())
-    dc = dircmp(record, test_subject)
+    dc = dircmp(record, test_subject, ignore=[".ruff_cache"])
     missing_files = dc.left_only + dc.right_only
     if missing_files:
         pytest.fail(f"{first_printable} or {second_printable} was missing: {missing_files}", pytrace=False)
