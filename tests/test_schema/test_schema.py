a9026168faa2fb55e5cb72a514b7f0f5a6cce1c2
Dylan Anthony <dbanty@users.noreply.github.com>
2023-10-15 15:21:43 -0600

    More nullable test coverage

--- tests/test_schema/test_schema.py
+++ tests/test_schema/test_schema.py
@@ -10,3 +10,18 @@ def test_nullable_with_allof():
     schema = Schema.model_validate_json('{"allOf": [{"type": "string"}], "nullable": true}')
     assert schema.oneOf == [Schema(type=DataType.NULL), Schema(allOf=[Schema(type=DataType.STRING)])]
     assert schema.allOf == []
+
+
+def test_nullable_with_type_list():
+    schema = Schema.model_validate_json('{"type": ["string", "number"], "nullable": true}')
+    assert schema.type == [DataType.STRING, DataType.NUMBER, DataType.NULL]
+
+
+def test_nullable_with_any_of():
+    schema = Schema.model_validate_json('{"anyOf": [{"type": "string"}], "nullable": true}')
+    assert schema.anyOf == [Schema(type=DataType.STRING), Schema(type=DataType.NULL)]
+
+
+def test_nullable_with_one_of():
+    schema = Schema.model_validate_json('{"oneOf": [{"type": "string"}], "nullable": true}')
+    assert schema.oneOf == [Schema(type=DataType.STRING), Schema(type=DataType.NULL)]
