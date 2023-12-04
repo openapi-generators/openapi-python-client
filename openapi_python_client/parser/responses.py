08a1f77bff7953f9d5acfd59db8797fd0d575a58
Dylan Anthony <dbanty@users.noreply.github.com>
2023-12-04 14:14:14 -0700

    Add tests, fix default content-type issue.

--- openapi_python_client/parser/responses.py
+++ openapi_python_client/parser/responses.py
@@ -24,24 +24,30 @@ class Response:
 
 
 def _source_by_content_type(content_type: str) -> Optional[str]:
-    content_type = utils.get_content_type(content_type)
+    parsed_content_type = utils.get_content_type(content_type)
+    if parsed_content_type is None:
+        return None
 
-    if content_type.startswith("text/"):
+    if parsed_content_type.startswith("text/"):
         return "response.text"
 
     known_content_types = {
         "application/json": "response.json()",
         "application/octet-stream": "response.content",
     }
-    source = known_content_types.get(content_type)
-    if source is None and content_type.endswith("+json"):
+    source = known_content_types.get(parsed_content_type)
+    if source is None and parsed_content_type.endswith("+json"):
         # Implements https://www.rfc-editor.org/rfc/rfc6838#section-4.2.8 for the +json suffix
         source = "response.json()"
     return source
 
 
 def empty_response(
-    *, status_code: HTTPStatus, response_name: str, config: Config, description: Optional[str]
+    *,
+    status_code: HTTPStatus,
+    response_name: str,
+    config: Config,
+    description: Optional[str],
 ) -> Response:
     """Return an untyped response, for when no response type is defined"""
     return Response(
@@ -72,7 +78,12 @@ def response_from_data(
     response_name = f"response_{status_code}"
     if isinstance(data, oai.Reference):
         return (
-            empty_response(status_code=status_code, response_name=response_name, config=config, description=None),
+            empty_response(
+                status_code=status_code,
+                response_name=response_name,
+                config=config,
+                description=None,
+            ),
             schemas,
         )
 
@@ -80,7 +91,10 @@ def response_from_data(
     if not content:
         return (
             empty_response(
-                status_code=status_code, response_name=response_name, config=config, description=data.description
+                status_code=status_code,
+                response_name=response_name,
+                config=config,
+                description=data.description,
             ),
             schemas,
         )
@@ -91,12 +105,18 @@ def response_from_data(
             schema_data = media_type.media_type_schema
             break
     else:
-        return ParseError(data=data, detail=f"Unsupported content_type {content}"), schemas
+        return (
+            ParseError(data=data, detail=f"Unsupported content_type {content}"),
+            schemas,
+        )
 
     if schema_data is None:
         return (
             empty_response(
-                status_code=status_code, response_name=response_name, config=config, description=data.description
+                status_code=status_code,
+                response_name=response_name,
+                config=config,
+                description=data.description,
             ),
             schemas,
         )
