a8b3e9027eb6c47cac68393478340d6f02c91020
Dylan Anthony <dbanty@users.noreply.github.com>
2024-03-05 18:58:31 -0700

    test: Add e2e test

--- end_to_end_tests/custom-templates-golden-record/my_test_api_client/api/naming/__init__.py
+++ end_to_end_tests/custom-templates-golden-record/my_test_api_client/api/naming/__init__.py
@@ -2,7 +2,7 @@
 
 import types
 
-from . import mixed_case, post_naming_property_conflict_with_import
+from . import hyphen_in_path, mixed_case, post_naming_property_conflict_with_import
 
 
 class NamingEndpoints:
@@ -13,3 +13,7 @@ class NamingEndpoints:
     @classmethod
     def mixed_case(cls) -> types.ModuleType:
         return mixed_case
+
+    @classmethod
+    def hyphen_in_path(cls) -> types.ModuleType:
+        return hyphen_in_path
