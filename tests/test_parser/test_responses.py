2edc6fe5e3838722aeb8f9075828f7ec880c3b3b
Eli Bishop <eli.bishop@benchling.com>
2024-10-25 10:24:49 -0700

    test coverage

--- tests/test_parser/test_responses.py
+++ tests/test_parser/test_responses.py
@@ -184,14 +184,14 @@ def test_response_from_data_reference(mocker, any_property_factory):
 
 
 @pytest.mark.parametrize(
-    "ref_string",
+    "ref_string,expected_error_string",
     [
-        "#/components/responses/Nonexistent",
-        "malformed-references-string",
-        "#/components/something-that-isnt-responses/ErrorResponse",
+        ("#/components/responses/Nonexistent", "Could not find"),
+        ("https://remote-reference", "Remote references"),
+        ("#/components/something-that-isnt-responses/ErrorResponse", "not allowed in responses"),
     ],
 )
-def test_response_from_data_reference_errors(ref_string, mocker, any_property_factory):
+def test_response_from_data_invalid_reference(ref_string, expected_error_string, mocker, any_property_factory):
     from openapi_python_client.parser import responses
 
     prop = any_property_factory()
@@ -213,6 +213,38 @@ def test_response_from_data_reference_errors(ref_string, mocker, any_property_fa
     )
 
     assert isinstance(response, ParseError)
+    assert expected_error_string in response.detail
+
+
+def test_response_from_data_ref_to_response_that_is_a_ref(mocker, any_property_factory):
+    from openapi_python_client.parser import responses
+
+    prop = any_property_factory()
+    mocker.patch.object(responses, "property_from_data", return_value=(prop, Schemas()))
+    predefined_response_base_data = oai.Response.model_construct(
+        description="",
+        content={"application/json": oai.MediaType.model_construct(media_type_schema="something")},
+    )
+    predefined_response_data = oai.Reference.model_construct(
+        ref="#/components/references/BaseResponse",
+    )
+    config = MagicMock()
+    config.content_type_overrides = {}
+
+    response, schemas = responses.response_from_data(
+        status_code=400,
+        data=oai.Reference.model_construct(ref="#/components/schemas/ErrorResponse"),
+        schemas=Schemas(),
+        responses={
+            "BaseResponse": predefined_response_base_data,
+            "ErrorResponse": predefined_response_data,
+        },
+        parent_name="parent",
+        config=config,
+    )
+
+    assert isinstance(response, ParseError)
+    assert "not allowed in responses" in response.detail
 
 
 def test_response_from_data_content_type_overrides(any_property_factory):
