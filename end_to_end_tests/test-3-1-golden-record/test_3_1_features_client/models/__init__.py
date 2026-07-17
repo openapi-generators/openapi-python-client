314e6522ba6677da5513656c67f7769d8c0321c9
Max Linke <max.linke@checkmk.com>
2026-07-17 08:31:17 +0200

    fix: cover content-only parameters in 3.1 snapshots

--- end_to_end_tests/test-3-1-golden-record/test_3_1_features_client/models/__init__.py
+++ end_to_end_tests/test-3-1-golden-record/test_3_1_features_client/models/__init__.py
@@ -1,9 +1,11 @@
 """Contains all the data models used in inputs/outputs"""
 
+from .get_content_item_id_item_id import GetContentItemIdItemId
 from .post_const_path_body import PostConstPathBody
 from .post_prefix_items_body import PostPrefixItemsBody
 
 __all__ = (
+    "GetContentItemIdItemId",
     "PostConstPathBody",
     "PostPrefixItemsBody",
 )
